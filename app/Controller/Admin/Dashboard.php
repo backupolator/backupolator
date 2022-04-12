3ef657b49682c89e0aa5cd08951e93f30331104a
majeed21 <majeedmohammadian@gmail.com>
2022-04-12 12:25:52 +0430

    add dump sql

--- app/Controller/Admin/Dashboard.php
+++ app/Controller/Admin/Dashboard.php
@@ -4,6 +4,8 @@ namespace Backupolator\Controller\Admin;
 
 use Backupolator\Helper\View;
 use SmartyException;
+use Rah\Danpu\Dump;
+use Rah\Danpu\Export;
 
 class Dashboard
 {
@@ -15,6 +17,20 @@ class Dashboard
      */
     public function index(): string
     {
+        try {
+            $dump = new Dump;
+            $dump
+                ->file(BACKUPOLATOR_PATH . DIRECTORY_SEPARATOR . 'backup' . DIRECTORY_SEPARATOR . 'file.sql')
+                ->dsn('mysql:dbname=' . DB_NAME . ';host=' . DB_HOST)
+                ->user(DB_USER)
+                ->pass(DB_PASSWORD)
+                ->tmp(BACKUPOLATOR_PATH . DIRECTORY_SEPARATOR . 'backup');
+
+            new Export($dump);
+        } catch (\Exception $e) {
+            echo 'Export failed with message: ' . $e->getMessage();
+        }
+
         wp_enqueue_script('backupolator-admin-dashboard');
         wp_enqueue_style('backupolator-admin-dashboard');
 
